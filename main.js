b60863341684878b65c2cd450f9b2946f019d852
Jobin Jose <45217928+The-Jin-799@users.noreply.github.com>
2021-08-25 13:29:18 +0530

    ffunctionaries

---
+++ main.js
@@ -0,0 +1,83 @@
+function today()
+{
+  var d= new Date()
+  var date = d.getDate()
+//conver day number to day name array
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var day = weekday[d.getDay()];
+  var year = d.getFullYear();
+  var hour = d.getHours()
+  var min = d.getMinutes()
+  var timeZone = d.toTimeString().slice(9) +" "+ Intl.DateTimeFormat().resolvedOptions().timeZone
+  var offset=d.getTimezoneOffset()
+
+  this.date = date
+  this.day = day
+  this.year = year
+  this.hour = hour
+  this.min = min 
+  this.timeZone = timeZone
+  this.time =`${d.toLocaleTimeString()} ${timeZone} `
+  this.timeoffset=offset
+ 
+}
+window.onpaint = currentTime();
+
+//showing sytem time 
+function currentTime()
+{
+  const Today= new today()
+  var nowTime = document.querySelector(".livetime P")
+  nowTime.innerText= Today.time
+}
+setInterval(currentTime,1000)
+
+//filling URL on box
+function fillFullURL(queryString){
+  if(!window.location.search){
+    var link=document.querySelector("#link")
+    var url=window.location.origin + "/timed.html?" + queryString;
+    link.value=url
+  }
+
+}
+ 
+var formObject={}
+
+const timeForm = document.forms.timeform;
+//on submission
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  for(let key of formData.keys())
+  {  
+    formObject[key]=formData.get(key)
+  }
+  var submittedDate = new Date(formObject.date+'T'+formObject.time)
+  //Convert submittted date and time to UTC
+  var localTime = submittedDate.getTime()
+  var localOffset = submittedDate.getTimezoneOffset()*60000
+  var utc = localTime + localOffset
+  //generating query string with utc and msg
+  queryObject={
+    "utc":utc,
+    "msg":formObject.message
+  }
+  const queryString = new URLSearchParams(queryObject);
+  fillFullURL(queryString);
+  console.log(queryString)
+
+
+
+}
+
+timeForm.addEventListener('submit', handleSubmit);
+
---
+++ timed.js
@@ -0,0 +1,15 @@
+var queryString= new URLSearchParams(document.location.search)
+var queryObject = {}
+
+for(var key of queryString.keys())
+{
+    queryObject[key]=queryString.get(key)
+}
+
+utc=Number(queryObject.utc)
+var viewerDate = new Date()
+var viewerOffSet= viewerDate.getTimezoneOffset()
+viewerOffSet*=60000
+var localTimeMs=utc-viewerOffSet
+var localDate= new Date(localTimeMs)
+console.log(localDate)
