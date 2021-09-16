36440323f9fe121941ed6b2b249af8cc152b7273
Jobin Jose <jobin.jose928@gmail.com>
2021-09-16 13:31:05 +0530

    timed styles

--- timed.js
+++ timed.js
@@ -15,10 +15,36 @@ var viewerDate = new Date(utc)
 viewerOffSet*=60000
 var localTimeMs=utc - viewerOffSet
 var localDate= new Date(localTimeMs)*/
+var userdate=viewerDate.getDate()
+var useryear=viewerDate.getFullYear()
+var usermonth=viewerDate.getMonth()
+var usertime=viewerDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+var months={
+    '0':"January",
+    '1':"February",
+    '2':"March",
+    '3':"April",
+    '4':"May",
+    '5':"June",
+    '6':"July",
+    '7':"August",
+    '8':"September",
+    '9':"October",
+    '10':"November",
+    '11':"December"
+}
 
-viewUpdatedTime = document.getElementById("time")
-viewUpdatedTime.innerText=viewerDate.toLocaleString()
-var message=queryObject.msg
-var msg= document.getElementById("msg")
-msg.innerText=message
+var date=document.getElementById("date")
+var year=document.getElementById("year")
+var month=document.getElementById("month")
+var time=document.getElementById("time")
+var message= document.getElementById("message")
+
+date.innerText=userdate
+year.innerText=useryear
+month.innerText=months[usermonth]
+time.innerText=usertime
+var msg=queryObject.msg
+message.innerText=msg
+
 console.log(viewerDate)
